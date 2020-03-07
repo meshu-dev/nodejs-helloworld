1309b419c2ea74f5217736b6166b97bbc7a102e0
Ubuntu <ubuntu@ip-172-26-3-65.eu-west-2.compute.internal>
2020-03-07 09:51:39 +0000

    Added server files

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '127.0.0.1';
+
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World! This is NodeJS running');
+});
+
+app.listen(PORT, HOST);
+
+console.log(`Running on http://${HOST}:${PORT}`);
+
